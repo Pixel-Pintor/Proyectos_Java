dfa075a96a4d7f76c50d369cb3f2478c581b6ccb
Christian <cpradavargas@gmail.com>
2022-06-16 12:12:00 -0500

    Terminado el metod que verifica el juego vertical

--- 01_Simple-Tick-Tack-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/kotlin/TicTacToe.java
+++ 01_Simple-Tick-Tack-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/kotlin/TicTacToe.java
@@ -1,6 +1,7 @@
 package tictactoe.kotlin;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -8,15 +9,20 @@ public class TicTacToe {
     final char DASH = '-';
     final char PLEAT = '|';
     final char SPACE = ' ';
+    final char OU = 'O';
+    final char EX = 'X';
     final int COLS = 8;
     final int ROWS = 4;
     final Scanner scanner = new Scanner(System.in);
+    final List<Integer> indexCol = List.of(2, 4, 6);
+    final List<Integer> indexRow = List.of(1, 2, 3);
 
     ArrayList<ArrayList<Character>> boardList;
 
     public void startTicTacToeGame() {
         boardList = createBoard(getUserInput());
         printGameBoard(boardList);
+        System.out.println(findVerticalGame(boardList));
     }
 
     private String getUserInput() {
@@ -35,7 +41,7 @@ public class TicTacToe {
                     listRow.add(DASH);
                 } else if (j == 0 || j == COLS) {
                     listRow.add(PLEAT);
-                } else if (j % 2 == 0) {
+                } else if (indexCol.contains(j)) {
                     listRow.add(inputArray[index++]);
                 } else {
                     listRow.add(SPACE);
@@ -55,15 +61,76 @@ public class TicTacToe {
         }
     }
 
-    private String[] findVerticalGame(ArrayList<ArrayList<Character>> boardList) {
-
+    private ArrayList<Character> findVerticalGame(ArrayList<ArrayList<Character>> boardList) {
+        // capturar caracter inicial y la columna
+        char firstChar;
+        ArrayList<Character> charArray = new ArrayList<>();
+        charArray.add(SPACE);
+        for (int i = 0; i < 1; i++) { // estoy en la primera fila
+            for (int j = 0; j < 3; j++) { // recorre las tres columnas
+                firstChar = boardList.get(indexRow.get(i)).get(indexCol.get(j)); // obtiene el caracter
+                if (firstChar == OU || firstChar == EX) {
+                    if (firstChar == boardList.get(indexRow.get(i) + 1).get(indexCol.get(j))) { // si el primer caracter es igual al segundo y al tercero
+                        if (firstChar == boardList.get(indexRow.get(i) + 2).get(indexCol.get(j))) {
+                            charArray.add(firstChar); // agrega el caracter si existe juego vertical
+                        }
+                    }
+                }
+            }
+        }
+        return charArray;
     }
 
-    private String[] findHorizontalGame(ArrayList<ArrayList<Character>> boardList) {
-
+    private ArrayList<Character> findHorizontalGame(ArrayList<ArrayList<Character>> boardList) {
+        char firstChar;
+        ArrayList<Character> charArray = new ArrayList<>();
+        charArray.add(SPACE);
+        for (ArrayList<Character> characters : boardList) {
+            for (int j = 0; j < 1; j++) {
+                firstChar = characters.get(j);
+                if (firstChar == OU || firstChar == EX) {
+                    if (firstChar == characters.get(j + 1)) {
+                        if (firstChar == characters.get(j + 2)) {
+                            charArray.add(firstChar);
+                        }
+                    }
+                }
+            }
+        }
+        return charArray;
     }
 
-    private String[] findDiagonalGame(ArrayList<ArrayList<Character>> boardList) {
-        
+    private char findDiagonalGame(ArrayList<ArrayList<Character>> boardList) {
+        // verifica si existe un juego diagonal descendente
+        char firstChar;
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 1; j++) {
+                firstChar = boardList.get(i).get(j);
+                if (firstChar == OU || firstChar == EX) {
+                    if (firstChar == boardList.get(i + 1).get(j + 1)) {
+                        if (firstChar == boardList.get(i + 2).get(j + 2)) {
+                            return firstChar;
+                        }
+                    }
+                }
+            }
+        }
+
+        // verifica si existe un juego diagonal ascendente
+        for (int i = 0; i < 1; i++) {
+            for (int j = 2; j > 1; j--) {
+                firstChar = boardList.get(i).get(j);
+                if (firstChar == OU || firstChar == EX) {
+                    if (firstChar == boardList.get(i + 1).get(j - 1)) {
+                        if (firstChar == boardList.get(i + 2).get(j - 2)) {
+                            return firstChar;
+                        }
+                    }
+                }
+            }
+        }
+
+        // retorna el caracter ganador o espacio si no hay ninguno
+        return SPACE;
     }
 }
